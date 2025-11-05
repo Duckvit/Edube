7ab6d7ecb9794698424f5c8205e302304a62be81
Duckvit <vietnd2004@gmail.com>
2025-11-05 14:54:19 +0700

    fix login/ sign up

--- Frontend/src/apis/MentorServices.jsx
+++ Frontend/src/apis/MentorServices.jsx
@@ -1,24 +1,26 @@
 import axiosConfig from "../axiosConfig";
 
-export const createMentor = async (data, token) => {
+export const createMentor = async (data) => {
   // Token will be automatically added by axiosConfig interceptor from localStorage
-  const res = await axiosConfig.post(`/api/mentors`, data, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const res = await axiosConfig.post(`/api/auth/mentors`, data);
   return res.data;
 };
 
 export const approveMentor = async (mentorId, token) => {
-  const res = await axiosConfig.put(`/api/mentors/${mentorId}/approve`, {}, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  const res = await axiosConfig.put(
+    `/api/mentors/${mentorId}/approve`,
+    {},
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
   return res.data;
 };
 
 export const getLearner = async (page = 0, size = 10, mentorId, token) => {
-  const res = await axiosConfig.get(`/api/mentors/${mentorId}/learners`,{
-    params: {page, size},
+  const res = await axiosConfig.get(`/api/mentors/${mentorId}/learners`, {
+    params: { page, size },
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
-}
+};
--- Frontend/src/utils/theme.js
+++ Frontend/src/utils/theme.js
@@ -150,3 +150,4 @@ export const componentStyles = {
 };
 
 export default theme;
+
